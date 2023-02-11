98b566ed6243de8a1398b9204f6b04542c1eb642
Ricardo Hoyos <rimaylu@gmail.com>
2023-02-10 23:05:46 -0500

    modification x3 commit

--- src/utils/handleResponses.js
+++ src/utils/handleResponses.js
@@ -24,3 +24,52 @@ module.exports = {
     success,
     error
 }
+
+/*
+const getAllProducts = (req, res) => {
+    findAllProcts()
+        .then(data => {
+            success({
+                res,
+                data,
+                status: 200,
+                message: 'All products collected succesfully'
+            })
+        })
+        .catch(err => {
+            error({
+                res,
+                data: err,
+                status: 400,
+                message: 'Se produjo un error al mostrar todos los productos',
+            })
+        })
+}
+const getProductById = (req, res) => {
+    findProductById(id)
+        .then(data => {
+            if(data){
+                success({
+                    res,
+                    data,
+                    status: 200,
+                    message: 'Product with id' + data.id
+                })
+            } else {
+                error({
+                    res,
+                    status: 404,
+                    message: 'Producto no encontrado'
+                })
+            }
+        })
+        .catch(err => {
+            error({
+                res,
+                data: err,
+                status: 400,
+                message: 'Se produjo un error al mostrar un producto',
+            })
+        })
+}
+*/
